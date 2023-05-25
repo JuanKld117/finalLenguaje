f937cc92d5ed1c540eec0daa719fe918f667a00e
JuanC <133279376+JuanKld117@users.noreply.github.com>
2023-05-25 15:02:46 -0600

    final

---
+++ examenfinall/src/Main.java
@@ -0,0 +1,10 @@
+import static models.principal.totalCigarros;
+
+public class Main {
+    public static void main(String[] args) {
+        presentacion.author071086();
+        totalCigarros();
+
+    }
+
+}
---
+++ examenfinall/src/presentacion.java
@@ -0,0 +1,8 @@
+public class presentacion {
+    public static void author071086() {
+        String nombre = "Juan J Lopez Calderon";
+        int matricula = 71086;
+        String correo = "al071086@uacam.mx";
+        System.out.println(nombre + "\n" + matricula + "\n" +correo);
+    }
+}
